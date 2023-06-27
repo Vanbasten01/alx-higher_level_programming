f016beea1cffcd6879ab7741f33fc6709a35cf20
Vanbasten01 <fouad.yasin01@gmail.com>
2023-06-27 15:26:30 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining a Square class """
+
+
+class Square:
+
+    """ Square class """
+    def __init__(self, size):
+        """ initializing a Square class """
+        self.__size = size
