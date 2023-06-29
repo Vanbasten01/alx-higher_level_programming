c02eddb6bd6cc591c28add33d438586f628a3f3a
Vanbasten01 <fouad.yasin01@gmail.com>
2023-06-29 19:57:55 +0100

    tst

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,25 +1,82 @@
 #!/usr/bin/python3
+"""
+Singly linked list Module
+"""
+
+
 class Node:
+
+    """
+    Node class
+    Attributes:
+        data (int): the data stored in the node
+        next_node (Node): a pointer to the next node
+        in the linked list
+
+    """
     def __init__(self, data, next_node=None):
+
+        """
+        Class initializer
+
+        Args:
+            data (int): the data stored in the node.
+            next_node (Node): a pointer to the next node
+
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Getter of the attribute data
+
+        Returns:
+            data: the stored data
+
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter of the attribure data
+
+        Args:
+            value (int): the given data
+
+        Raises:
+            TypeError: if value is not integer
+
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        Getter of the next_node
+
+        Returns:
+            next_node: a pointer to the next node
+
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Setter of the next_node
+
+        Args:
+            value (Node): a pointer to the next node
+
+        Raises:
+            TypeError: if value not node or None
+
+        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         else:
@@ -27,10 +84,23 @@ class Node:
 
 
 class SinglyLinkedList:
+    """
+    SiglyLinkedList defines the singly linked list
+
+    Attributes:
+        head (Node): a pointer to the singly linked list
+
+    """
     def __init__(self):
+        """
+        Class initializer
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts new Node into the correct soeted position
+        """
         new = Node(value)
         if self.__head is None:
             new.next_node = None
