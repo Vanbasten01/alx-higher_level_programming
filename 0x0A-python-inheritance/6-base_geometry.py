827fc20da0e2e558ab105453caf4319b3a6a19ce
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-10 20:08:45 +0100

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a mod that contains an empty class "BaseGeometry"."""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """a public instance method """
+        raise Exception("area() is not implemented")
