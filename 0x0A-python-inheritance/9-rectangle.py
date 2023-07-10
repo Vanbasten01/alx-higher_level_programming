57ae927989eb7f6e1713d99b8ba9cf149f018bbf
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-10 21:42:54 +0100

    Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" a mod that contains an empty class "BaseGeometry"."""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """a public instance method """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ a method that validates an integer.
+
+        args:
+            name (str) : the given Name.
+            value (int) : the given Value.
+        raise:
+            TypeError: if "value" is not an int.
+            ValueError: if value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ a class that inherites from BaseGeometry """
+    def __init__(self, width, height):
+        """intializer.
+        args:
+            width (int): the width of the rectangle.
+            height (int):
+            the height of the rectangle.
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ a method that returns the rectangle's area"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
