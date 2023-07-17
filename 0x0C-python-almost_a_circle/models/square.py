ea2586a8c2e2bf6c15c2f08b34a39efa135ea98a
Vanbasten01 <fouad.yasin01@gmail.com>
2023-07-17 21:07:53 +0100

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,14 +4,18 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Square class which inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initializing a Square instance"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """a method that returns the size"""
         return self.width
 
     @size.setter
+    """ a setter method that sets the the size"""
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -21,9 +25,19 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
+        """it returns the string representation of an object """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     def update(self, *args, **kwargs):
+        """
+        a methof that Updates the rectangle's attributes.
+
+        Args:
+            *args: The positional arguments that can be used to update id,
+                   width, height, x, and y in that order.
+            **kwargs: The keyword arguments that can be used to update any
+                   attribute by specifying the attribute name.
+        """
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -38,4 +52,5 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ amethod that converts the attributtes to a dictionary """
         return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Unit tests for testing the Base class."""
 
 import unittest
 
@@ -6,18 +7,22 @@ from models.base import Base
 
 
 class TestBaseClass(unittest.TestCase):
+    """Unit tests for testing the Base class."""
 
     def test_base_1_using_id(self):
+        """""Test initializing Base instance with a specific ID."""
         b1 = Base(344)
         self.assertEqual(b1.id, 344)
 
     def test_base_2_id_default(self):
+        """Test initializing Base instance with default id."""
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
     def test_base_3_many_instances(self):
+        """test multiple instances"""
         b3 = Base()
         b4 = Base(23)
         b5 = Base()
@@ -26,21 +31,30 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(b5.id, 4)
 
     def test_base_4_to_json_string_method(self):
+        """test to_json_string method """
         self.assertEqual(Base.to_json_string([]), "[]")
         ld1 = Base.to_json_string([{'id': 34, 'name': True}])
         self.assertEqual(ld1, '[{"id": 34, "name": true}]')
-        ld2 = Base.to_json_string([{"id": 34, "name": "Yasin"},
-                                    {"id": 28, "name": "Vanbasten"}])
-        self.assertEqual(ld2, '[{"id": 34, "name": "Yasin"}, '
-                                '{"id": 28, "name": "Vanbasten"}]')
+        ld2 = Base.to_json_string([
+            {"id": 34, "name": "Yasin"},
+            {"id": 28, "name": "Vanbasten"}
+            ])
+        self.assertEqual(
+                ld2, '[{"id": 34, "name": "Yasin"}, '
+                '{"id": 28, "name": "Vanbasten"}]'
+                )
         self.assertIsInstance(ld2, str)
 
     def test_base_5_from_json_string_method(self):
+        """test the from_json_string """
         self.assertEqual(Base.to_json_string(""), "[]")
         self.assertEqual(Base.to_json_string(None), "[]")
         ld1 = Base.from_json_string('[{"id": 34, "name": true}]')
         self.assertEqual(ld1, [{'id': 34, 'name': True}])
-        ld2 = Base.from_json_string('[{"id": 34, "name": "Yasin"}, '
-                                     '{"id": 28, "name": "Vanbasten"}]')
-        self.assertEqual(ld2, [{"id": 34, "name": "Yasin"},
-                                {"id": 28, "name": "Vanbasten"}])
+        ld2 = Base.from_json_string(
+                '[{"id": 34, "name": "Yasin"}, '
+                '{"id": 28, "name": "Vanbasten"}]'
+                )
+        self.assertEqual(ld2, [{
+            "id": 34, "name": "Yasin"},
+                {"id": 28, "name": "Vanbasten"}])
