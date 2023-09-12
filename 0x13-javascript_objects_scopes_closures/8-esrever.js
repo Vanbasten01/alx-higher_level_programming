bc137a96fd6d13b1419c09d09caed25c86d79ce5
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-12 17:57:15 +0100

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const rev = [];
+  const len = list.length;
+
+  for (let i = len - 1; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+  return rev;
+};
