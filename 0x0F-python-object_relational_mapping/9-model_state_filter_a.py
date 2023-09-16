1214a751e6c0889dd56a811a4e0fcb98fde95f40
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-17 00:01:18 +0100

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,24 +7,24 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(username, password, db_name),
-                       pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_name_a = (
+            session.query(State)
+            .filter(State.name.like('%a%'))
+            .order_by(State.id).all()
+    )
 
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-states_name_a = (
-        session.query(State)
-        .filter(State.name.like('%a%'))
-        .order_by(State.id).all()
-)
-
-for state in states_name_a:
-    print(state.id, state.name, sep=': ')
-session.close()
+    for state in states_name_a:
+        print(state.id, state.name, sep=': ')
+    session.close()
