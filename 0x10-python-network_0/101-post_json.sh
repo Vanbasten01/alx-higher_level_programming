776ced8009cdeb711831060fbfec4f4d15ca554e
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-28 22:25:10 +0100

    Advanced Task 2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -X POST $1 -H "content-type: application/json" -d @$2
+curl -sX POST "$1" -H "content-type: application/json" -d @"$2"
