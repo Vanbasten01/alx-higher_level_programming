e5cee05f88d1e390c556a27a1ab4e36ae65cf8a4
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-29 03:26:11 +0100

    Task 6  Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,24 @@
 """a Module that defines "find_peak" function"""
 
 
+def binary_peak_finder(my_list, left, right):
+    """A helper function to find a peak using binary search"""
+    if left == right:
+        return my_list[left]
+    mid = (left + right) // 2
+    if my_list[mid + 1] > my_list[mid]:
+        return binary_peak_finder(my_list, mid + 1, right)
+    else:
+        return binary_peak_finder(my_list, left, mid)
+
+
 def find_peak(list_of_integers):
     """ a function that finds a peak
         in a list of unsorted integers.
     """
-    if not list_of_integers:
-        return None
-    my_list = list_of_integers[:]
-    if len(my_list) == 1:
-        return my_list[0]
-    if len(my_list) == 2:
-        return max(my_list)
-    prev = 0
-    for index, num in enumerate(my_list):
-        if index:
-            prev = my_list[index - 1]
-        if index < len(my_list) - 1:
-            nxt = my_list[index + 1]
-        if prev <= num >= nxt:
-            return num
+    n = len(list_of_integers)
+    if n == 0:
+        return
+    if n == 1:
+        return list_of_integers[1]
+    return binary_peak_finder(list_of_integers, 0, n - 1)
