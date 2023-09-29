be614c3b0161dfb875a6be0eb15f7ca18c9bc607
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-30 00:53:18 +0100

    tst

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,16 +3,17 @@
 import requests
 import sys
 
-repo = sys.argv[1]
-owner = sys.argv[2]
-r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
-if r.status_code == 200:
-    commits = r.json()
-    count = 0
-    for commit in commits:
-        commit_sha = commit.get('sha')
-        commit_author = commit.get('commit').get('author').get('name')
-        print(f"{commit_sha} {commit_author}")
-        count += 1
-        if count == 10:
-            break
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+    if r.status_code == 200:
+        commits = r.json()
+        count = 0
+        for commit in commits:
+            commit_sha = commit.get('sha')
+            commit_author = commit.get('commit').get('author').get('name')
+            print(f"{commit_sha} {commit_author}")
+            count += 1
+            if count == 10:
+                break
