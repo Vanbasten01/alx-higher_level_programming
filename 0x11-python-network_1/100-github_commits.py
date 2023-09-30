bb7cd4c57677123ba44b4e8d5cd852c66840b699
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-30 01:16:01 +0100

    The Advanced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that takes 2 arguments in order to solve this challenge."""
+"""script that takes 2 arguments (the repo name and the owner)
+    in order to return the last 10 commit to the specified repo"""
 import requests
 import sys
 
@@ -7,13 +8,8 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
-    if r.status_code == 200:
-        commits = r.json()
-        count = 0
-        for commit in commits:
-            commit_sha = commit.get('sha')
-            commit_author = commit.get('commit').get('author').get('name')
-            print(f"{commit_sha} {commit_author}")
-            count += 1
-            if count == 10:
-                break
+    commits = r.json()[:10]
+    for commit in commits:
+        commit_sha = commit.get('sha')
+        commit_name = commit.get('commit').get('author').get('name')
+        print(f"{commit_sha}: {commit_name}")
