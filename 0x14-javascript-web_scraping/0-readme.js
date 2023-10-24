40dec8c467beb1510889d5158432e465ff4023f4
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-24 20:02:43 +0100

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  (err) ? console.log(err) : console.log(data);
+});
