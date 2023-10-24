7475bd55715ede19e8b230d88c2779c60c17e019
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-24 22:57:07 +0100

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(filepath, body, 'utf-8', (err) => {
+      if (err) { console.error(err); }
+    });
+  }
+});
