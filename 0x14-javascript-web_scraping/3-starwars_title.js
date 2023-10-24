b2e9370286ebff350ab3df4629e7cfda93316cb3
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-24 21:53:49 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
